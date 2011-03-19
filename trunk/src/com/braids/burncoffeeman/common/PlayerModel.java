1cc595b305ec7ae6a9cb084c452f16a364fabbf4
gabor.papai <gabor.papai@17e6173b-5c23-b1be-a29a-61aff9fbf666>
2011-03-19 09:56:10 +0000

    ud

--- trunk/src/com/braids/burncoffeeman/common/PlayerModel.java
+++ trunk/src/com/braids/burncoffeeman/common/PlayerModel.java
@@ -1,22 +1,35 @@
 package com.braids.burncoffeeman.common;
 
-import java.nio.ByteBuffer;
 import java.util.EnumSet;
 
 public class PlayerModel implements CoderDecoder {
 
+	@Transfer(TransferType.AUTO)
+	PacketMessageType        packetMessageType = PacketMessageType.PLAYER_MODEL;
+	@Transfer(TransferType.BYTE)
 	int                      playerId;
+	@Transfer(TransferType.SHORT)
 	int                      x;
+	@Transfer(TransferType.SHORT)
 	int                      y;
+	@Transfer(TransferType.AUTO)
 	Direction                direction;
+	@Transfer(TransferType.AUTO)
 	Activity                 activity;
+	@Transfer(TransferType.BYTE)
 	int                      speed;
+	@Transfer(TransferType.BYTE)
 	int                      vitality;
+	@Transfer(TransferType.AUTO)
 	boolean                  isHurted;
+	@Transfer(TransferType.AUTO)
 	boolean                  isHurt;
+	@Transfer(TransferType.AUTO)
 	boolean                  isMagic;
+	@Transfer(TransferType.AUTO)
 	private EnumSet<Disease> disease;
 	/** divide it with 100. transferred as short-int */
+	@Transfer(TransferType.SHORT)
 	private int              animationPhase;
 
 	public PlayerModel() {
@@ -25,107 +38,44 @@ public class PlayerModel implements CoderDecoder {
 	}
 
 	public byte[] code() {
-		ByteBuffer bb = ByteBuffer.allocate(1 + 2 + 2 + 7 + 2);
+		CodecBuilder cb = new CodecBuilder();
+		cb.putEnum(PacketMessageType.PLAYER_MODEL);
+		cb.putByte(playerId);
+		cb.putShort(x);
+		cb.putShort(y);
+		cb.putEnum(direction);
+		cb.putEnum(activity);
+		cb.putByte(speed);
+		cb.putByte(vitality);
+		cb.putByte(((isHurted ? 1 : 0) + (isHurt ? 2 : 0) + (isMagic ? 4 : 0)));
+		cb.putEnumSet(disease, Disease.class);
+		cb.putShort(animationPhase);
 
-		bb.put((byte) PacketMessageType.PLAYER_MODEL.ordinal());
+		// FIXME
+		byte[] autoResult = CodecBuilder.auto(this);
 
-		Helper.putShortIntToBuffer(bb, x);
-		Helper.putShortIntToBuffer(bb, y);
-		bb.put((byte) direction.ordinal());
-		bb.put((byte) activity.ordinal());
-		bb.put((byte) speed);
-		bb.put((byte) vitality);
-		bb.put((byte) playerId);
-
-		byte effects = (byte) ((isHurted ? 1 : 0) + (isHurt ? 2 : 0) + (isMagic ? 4 : 0));
-
-		bb.put(effects);
-
-		byte diseaseByte = 0;
-		for (Disease d : disease) {
-			switch (d) {
-				case BOMB_SHITTING:
-					diseaseByte |= 1;
-					break;
-				case NO_BOMB:
-					diseaseByte |= 2;
-					break;
-				case COLOR_BLIND:
-					diseaseByte |= 4;
-					break;
-				case FAST_DETONATION:
-					diseaseByte |= 8;
-					break;
-				case RABBIT:
-					diseaseByte |= 16;
-					break;
-				case REVERSE:
-					diseaseByte |= 32;
-					break;
-				case SHORT_RANGE:
-					diseaseByte |= 64;
-					break;
-				case SLOW_MOTION:
-					diseaseByte |= 128;
-					break;
-			}
-		}
-		bb.put(diseaseByte);
-
-		Helper.putShortIntToBuffer(bb, animationPhase);
-
-		if (bb.position() != bb.capacity()) {
-			throw new IllegalStateException("Error in coder: " + bb.position() + " " + bb.capacity());
-		}
-
-		return bb.array();
+		return cb.getResult();
 	}
 
 	public int decode(byte[] bytes, int offset) {
 
-		x = Helper.bytesToInt(bytes[offset], bytes[offset + 1]);
-		y = Helper.bytesToInt(bytes[offset + 2], bytes[offset + 3]);
-		direction = Direction.values()[bytes[offset + 4]];
-		activity = Activity.values()[bytes[offset + 5]];
-		speed = bytes[offset + 6];
-		vitality = bytes[offset + 7];
-		playerId = bytes[offset + 8];
-		byte effects = bytes[offset + 9];
+		DecodecBuilder db = new DecodecBuilder(bytes, offset);
+
+		playerId = db.getByte();
+		x = db.getShort();
+		y = db.getShort();
+		direction = db.getEnum(Direction.class);
+		activity = db.getEnum(Activity.class);
+		speed = db.getByte();
+		vitality = db.getByte();
+		int effects = db.getByte();
 		isHurted = (effects & 1) != 0;
 		isHurt = (effects & 2) != 0;
 		isMagic = (effects & 4) != 0;
+		disease = db.getEnumSet(Disease.class);
+		animationPhase = db.getShort();
 
-		byte diseasesByte = bytes[offset + 10];
-		disease = EnumSet.noneOf(Disease.class);
-		if ((diseasesByte & 1) != 0) {
-			disease.add(Disease.BOMB_SHITTING);
-		}
-		if ((diseasesByte & 2) != 0) {
-			disease.add(Disease.NO_BOMB);
-		}
-		if ((diseasesByte & 4) != 0) {
-			disease.add(Disease.COLOR_BLIND);
-		}
-		if ((diseasesByte & 8) != 0) {
-			disease.add(Disease.FAST_DETONATION);
-		}
-		if ((diseasesByte & 16) != 0) {
-			disease.add(Disease.RABBIT);
-		}
-		if ((diseasesByte & 32) != 0) {
-			disease.add(Disease.REVERSE);
-		}
-		if ((diseasesByte & 64) != 0) {
-			disease.add(Disease.SHORT_RANGE);
-		}
-
-		if ((diseasesByte & 128) != 0) {
-			disease.add(Disease.SLOW_MOTION);
-		}
-
-		animationPhase = Helper.bytesToInt(bytes[offset + 11], bytes[offset + 12]);
-
-		return 13;
+		return db.getOffsetIncrement();
 	}
 
 	public int getPlayerId() {
